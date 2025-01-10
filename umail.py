552178c10b2cc71354387153dc0e96dbedb3c3b3
M1mbik <mikolaj.kowiak@gmail.com>
2025-01-10 12:58:24 +0100

    Update umail.py

--- umail.py
+++ umail.py
@@ -1,10 +1,8 @@
-# Complete project details: https://RandomNerdTutorials.com/micropython-send-emails-esp32-esp826/
-# uMail (MicroMail) for MicroPython
-# Copyright (c) 2018 Shawwwn <shawwwn1@gmai.com> https://github.com/shawwwn/uMail/blob/master/umail.py
-# License: MIT
 import usocket
+import ssl as ssl_module  # Zmieniamy nazwę importu, aby uniknąć kolizji
+import ubinascii
 
-DEFAULT_TIMEOUT = 10 # sec
+DEFAULT_TIMEOUT = 10  # sec
 LOCAL_DOMAIN = '127.0.0.1'
 CMD_EHLO = 'EHLO'
 CMD_STARTTLS = 'STARTTLS'
@@ -15,7 +13,8 @@ AUTH_LOGIN = 'LOGIN'
 
 class SMTP:
     def cmd(self, cmd_str):
-        sock = self._sock;
+        """Wysyła komendę do serwera i oczekuje odpowiedzi"""
+        sock = self._sock
         sock.write('%s\r\n' % cmd_str)
         resp = []
         next = True
@@ -26,85 +25,127 @@ class SMTP:
         return int(code), resp
 
     def __init__(self, host, port, ssl=False, username=None, password=None):
-        import ssl
-        self.username = username
-        addr = usocket.getaddrinfo(host, port)[0][-1]
-        sock = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
-        sock.settimeout(DEFAULT_TIMEOUT)
-        sock.connect(addr)
-        if ssl:
-            sock = ssl.wrap_socket(sock)
-        code = int(sock.read(3))
-        sock.readline()
-        assert code==220, 'cant connect to server %d, %s' % (code, resp)
-        self._sock = sock
+        try:
+            addr = usocket.getaddrinfo(host, port)[0][-1]
+            sock = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+            sock.settimeout(DEFAULT_TIMEOUT)  # Ustaw timeout
+            sock.connect(addr)
+            if ssl:
+                sock = ssl_module.wrap_socket(sock)  # Używamy ssl_module, aby uniknąć kolizji
+            code = int(sock.read(3))
+            sock.readline()
+            if code != 220:
+                raise ConnectionError(f'Błąd połączenia z serwerem: {code}')
+            
+            self._sock = sock
+            code, resp = self.cmd(CMD_EHLO + ' ' + LOCAL_DOMAIN)
+            if code != 250:
+                raise ConnectionError(f'Błąd EHLO: {code}')
 
-        code, resp = self.cmd(CMD_EHLO + ' ' + LOCAL_DOMAIN)
-        assert code==250, '%d' % code
-        if not ssl and CMD_STARTTLS in resp:
-            code, resp = self.cmd(CMD_STARTTLS)
-            assert code==220, 'start tls failed %d, %s' % (code, resp)
-            self._sock = ssl.wrap_socket(sock)
+            if not ssl and CMD_STARTTLS in resp:
+                code, resp = self.cmd(CMD_STARTTLS)
+                if code != 220:
+                    raise ConnectionError(f'Błąd STARTTLS: {code}')
+                self._sock = ssl_module.wrap_socket(sock)  # Używamy ssl_module, aby uniknąć kolizji
 
-        if username and password:
-            self.login(username, password)
+            if username and password:
+                self.login(username, password)
+        except Exception as e:
+            print(f"Nie udało się połączyć z serwerem: {e}")
+            if hasattr(self, '_sock') and self._sock:
+                self._sock.close()  # Zamykanie połączenia w przypadku błędu
+            raise
 
     def login(self, username, password):
-        self.username = username
-        code, resp = self.cmd(CMD_EHLO + ' ' + LOCAL_DOMAIN)
-        assert code==250, '%d, %s' % (code, resp)
+        try:
+            self.username = username
+            code, resp = self.cmd(CMD_EHLO + ' ' + LOCAL_DOMAIN)
+            if code != 250:
+                raise ConnectionError(f'Błąd EHLO: {code}')
+            
+            auths = None
+            for feature in resp:
+                if feature[:4].upper() == CMD_AUTH:
+                    auths = feature[4:].strip('=').upper().split()
+            if not auths:
+                raise Exception("Brak obsługi autentykacji.")
 
-        auths = None
-        for feature in resp:
-            if feature[:4].upper() == CMD_AUTH:
-                auths = feature[4:].strip('=').upper().split()
-        assert auths!=None, "no auth method"
-
-        from ubinascii import b2a_base64 as b64
-        if AUTH_PLAIN in auths:
-            cren = b64("\0%s\0%s" % (username, password))[:-1].decode()
-            code, resp = self.cmd('%s %s %s' % (CMD_AUTH, AUTH_PLAIN, cren))
-        elif AUTH_LOGIN in auths:
-            code, resp = self.cmd("%s %s %s" % (CMD_AUTH, AUTH_LOGIN, b64(username)[:-1].decode()))
-            assert code==334, 'wrong username %d, %s' % (code, resp)
-            code, resp = self.cmd(b64(password)[:-1].decode())
-        else:
-            raise Exception("auth(%s) not supported " % ', '.join(auths))
-
-        assert code==235 or code==503, 'auth error %d, %s' % (code, resp)
-        return code, resp
+            if AUTH_PLAIN in auths:
+                from ubinascii import b2a_base64 as b64
+                cren = b64(f"\0{username}\0{password}")[:-1].decode()
+                code, resp = self.cmd(f'{CMD_AUTH} {AUTH_PLAIN} {cren}')
+            elif AUTH_LOGIN in auths:
+                from ubinascii import b2a_base64 as b64
+                code, resp = self.cmd(f"{CMD_AUTH} {AUTH_LOGIN} {b64(username)[:-1].decode()}")
+                if code != 334:
+                    raise Exception(f'Błąd logowania: {code}')
+                code, resp = self.cmd(b64(password)[:-1].decode())
+            else:
+                raise Exception(f"Metoda autentykacji ({', '.join(auths)}) nie jest obsługiwana.")
+            
+            if code not in [235, 503]:
+                raise Exception(f'Błąd autentykacji: {code}')
+        except Exception as e:
+            print(f"Problem podczas logowania: {e}")
+            if hasattr(self, '_sock') and self._sock:
+                self._sock.close()  # Zamykanie połączenia w przypadku błędu
+            raise
 
     def to(self, addrs, mail_from=None):
-        mail_from = self.username if mail_from==None else mail_from
-        code, resp = self.cmd(CMD_EHLO + ' ' + LOCAL_DOMAIN)
-        assert code==250, '%d' % code
-        code, resp = self.cmd('MAIL FROM: <%s>' % mail_from)
-        assert code==250, 'sender refused %d, %s' % (code, resp)
+        try:
+            mail_from = self.username if mail_from is None else mail_from
+            code, resp = self.cmd(CMD_EHLO + ' ' + LOCAL_DOMAIN)
+            if code != 250:
+                raise Exception(f'Błąd EHLO: {code}')
+            code, resp = self.cmd(f'MAIL FROM: <{mail_from}>')
+            if code != 250:
+                raise Exception(f'Błąd nadawcy: {code}')
 
-        if isinstance(addrs, str):
-            addrs = [addrs]
-        count = 0
-        for addr in addrs:
-            code, resp = self.cmd('RCPT TO: <%s>' % addr)
-            if code!=250 and code!=251:
-                print('%s refused, %s' % (addr, resp))
-                count += 1
-        assert count!=len(addrs), 'recipient refused, %d, %s' % (code, resp)
+            if isinstance(addrs, str):
+                addrs = [addrs]
+            count = 0
+            for addr in addrs:
+                code, resp = self.cmd(f'RCPT TO: <{addr}>')
+                if code not in [250, 251]:
+                    print(f'{addr} odmówiono, {resp}')
+                    count += 1
+            if count == len(addrs):
+                raise Exception(f'Wszyscy odbiorcy odmówili, kod: {code}')
 
-        code, resp = self.cmd('DATA')
-        assert code==354, 'data refused, %d, %s' % (code, resp)
-        return code, resp
+            code, resp = self.cmd('DATA')
+            if code != 354:
+                raise Exception(f'Błąd przesyłania danych: {code}')
+        except Exception as e:
+            print(f"Błąd w trakcie wysyłania wiadomości: {e}")
+            if hasattr(self, '_sock') and self._sock:
+                self._sock.close()  # Zamykanie połączenia w przypadku błędu
+            raise
 
     def write(self, content):
+        """Wysyła treść wiadomości"""
         self._sock.write(content)
 
     def send(self, content=''):
-        if content:
-            self.write(content)
-        self._sock.write('\r\n.\r\n') # the five letter sequence marked for ending
-        line = self._sock.readline()
-        return (int(line[:3]), line[4:].strip().decode())
+        """Kończy wysyłanie wiadomości"""
+        try:
+            if content:
+                self.write(content)
+            self._sock.write('\r\n.\r\n')  # zakończenie wiadomości
+            line = self._sock.readline()
+            return int(line[:3]), line[4:].strip().decode()
+        except Exception as e:
+            print(f"Błąd podczas wysyłania wiadomości: {e}")
+            if hasattr(self, '_sock') and self._sock:
+                self._sock.close()  # Zamykanie połączenia w przypadku błędu
+            raise
 
     def quit(self):
-        self.cmd("QUIT")
-        self._sock.close()
+        """Kończy połączenie SMTP"""
+        try:
+            self.cmd("QUIT")
+            self._sock.close()
+        except Exception as e:
+            print(f"Błąd podczas zamykania połączenia: {e}")
+            if hasattr(self, '_sock') and self._sock:
+                self._sock.close()  # Zamykanie połączenia w przypadku błędu
+
